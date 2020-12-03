3457b08d70cb4225e40c407007710e21b0161342
eol morikvendy <emorikvendy@gmail.com>
2020-12-03 22:47:37 +0300

    three versions of fizz-buzz program

---
+++ lvl-1/lesson-3/FizzBuzz/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	thirdSolution()
+}
+
+func firstSolution() {
+	for i := 1; i < 101; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+func secondSolution() {
+	for i := 1; i < 101; i++ {
+		modulo := i % 15
+		switch modulo {
+		case 0:
+			fmt.Println("FizzBuzz")
+		case 3:
+			fallthrough
+		case 6:
+			fallthrough
+		case 9:
+			fallthrough
+		case 12:
+			fmt.Println("Fizz")
+		case 5:
+			fallthrough
+		case 10:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
+
+func thirdSolution() {
+	for i := 1; i < 101; i++ {
+		if i%5 == 0 {
+			if i%3 == 0 {
+				fmt.Print("Fizz")
+			}
+			fmt.Println("Buzz")
+
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
---
+++ lvl-1/lesson-3/FizzBuzz/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkFirst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		firstSolution()
+	}
+}
+
+func BenchmarkSecond(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		secondSolution()
+	}
+}
+
+func BenchmarkThird(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		thirdSolution()
+	}
+}
