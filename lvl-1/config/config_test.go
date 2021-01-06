85b503224e137e6170d06546cc98469c38f47375
eol morikvendy <emorikvendy@gmail.com>
2021-01-06 10:14:46 +0300

    Загрузка конфигурации из json/yaml/environment

---
+++ lvl-1/config/config_test.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+var valid = Config{
+	Port:        "8080",
+	DbUrl:       "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable",
+	JaegerUrl:   "http://jaeger:16686",
+	SentryUrl:   "http://sentry:9000",
+	KafkaBroker: "kafka:9092",
+	SomeAppId:   "testid",
+	SomeAppKey:  "testkey",
+}
+
+func TestFromYaml(t *testing.T) {
+	config, err := FromYaml("valid.yaml")
+	if err != nil {
+		t.Errorf("Неверный результат выполнения на valid.yaml: %v", err)
+	}
+	if reflect.DeepEqual(valid, config) {
+		t.Errorf("Неверный результат выполнения на valid.yaml: %v", config)
+	}
+	config, err = FromYaml("invalid.yaml")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на invalid.yaml: %v", config)
+	}
+	config, err = FromYaml("incomplete.yaml")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на incomplete.yaml: %v", config)
+	}
+	config, err = FromYaml("nonexistent.yaml")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на nonexistent.yaml: %v", config)
+	}
+}
+
+func TestFromJson(t *testing.T) {
+	config, err := FromJson("valid.json")
+	if err != nil {
+		t.Errorf("Неверный результат выполнения на valid.json: %v", err)
+	}
+	if reflect.DeepEqual(valid, config) {
+		t.Errorf("Неверный результат выполнения на valid.json: %v", config)
+	}
+	config, err = FromJson("invalid.json")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на invalid.json: %v", config)
+	}
+	config, err = FromJson("incomplete.json")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на incomplete.json: %v", config)
+	}
+	config, err = FromJson("nonexistent.json")
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на nonexistent.json: %v", config)
+	}
+}
+
+func TestFromEnvironment(t *testing.T) {
+	os.Setenv("PORT", "8080")
+	os.Setenv("DB_URL", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable")
+	os.Setenv("JAEGER_URL", "http://jaeger:16686")
+	os.Setenv("SENTRY_URL", "http://sentry:9000")
+	os.Setenv("KAFKA_BROKER", "kafka:9092")
+	config, err := FromEnvironment()
+	if err == nil {
+		t.Errorf("Неверный результат выполнения на неполных данных: %v", config)
+	}
+
+	os.Setenv("SOME_APP_ID", "testid")
+	os.Setenv("SOME_APP_KEY", "testkey")
+	config, err = FromEnvironment()
+	if err != nil {
+		t.Errorf("Неверный результат выполнения на полных данных: %v", err)
+	}
+	if reflect.DeepEqual(valid, config) {
+		t.Errorf("Неверный результат выполнения на полных данных: %v", config)
+	}
+}
