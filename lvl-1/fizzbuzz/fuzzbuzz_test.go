03f10236d7c7e30d410f5afa8762898ed6508008
eol morikvendy <emorikvendy@gmail.com>
2021-01-11 01:17:58 +0300

    тесты для простых чисел и бенчмарк для fuzzbuzz

--- lvl-1/fizzbuzz/fuzzbuzz_test.go
+++ lvl-1/fizzbuzz/fuzzbuzz_test.go
@@ -1,23 +1,24 @@
 package fuzzbuzz
 
 import (
+	"io/ioutil"
 	"testing"
 )
 
 func BenchmarkFirst(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		firstSolution()
+		firstSolution(ioutil.Discard)
 	}
 }
 
 func BenchmarkSecond(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		secondSolution()
+		secondSolution(ioutil.Discard)
 	}
 }
 
 func BenchmarkThird(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		thirdSolution()
+		thirdSolution(ioutil.Discard)
 	}
 }
--- lvl-1/main.go
+++ lvl-1/main.go
@@ -9,7 +9,7 @@ import (
 
 /*func main() {
 	fmt.Println("Введите номер числа Фибоначчи, которое хотите получить")
-	n := scan.Int64(os.Stdout)
+	n := scan.Int64(os.Stdout, os.Stdin)
 	fmt.Println("Loop result: ", fibonacci.Loop(n))
 	//fmt.Println("Recursive result: ", Fibonacci.Recursive(n))
 	fmt.Println("RecursiveWithMap result: ", fibonacci.RecursiveWithMap(n))
